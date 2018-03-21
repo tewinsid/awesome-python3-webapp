d49ef357ad5f463617c13addc4240f1bddabb284
tewinsid <vipwanggang@outlook.com>
2018-03-21 09:05:02 +0800

    20180321

--- src/coreweb.py
+++ src/coreweb.py
@@ -58,6 +58,25 @@ def has_named_kw_args(fn):
     params = inspect.signature(fn).parameters
     for name,param in params.items():
         if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            return True
+
+def has_var_kw_arg(fn):
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.VAR_KEYWORD
+            return True
+
+def has_request_arg(fn):
+    sig = inspect.signature(fn)
+    params = sig.parameters
+    found = False
+    for name,param in params.items():
+        if name == 'request':
+            found = True
+            continue
+        if found and (param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
+            raise ValueError('request parameter must the last named parameter in function:%s%s' % (fn.__name__, str(sig)))
+    return found
 
 
 #目的：从url中分析出需要接受的参数
@@ -66,11 +85,65 @@ class RequestHandler(object):
     def __init__(Self, app, fn):
         self._app = app
         self._func = fn
+        self._has_request_arg = has_request_arg(fn)
+        self._has_var_kw_arg = has_var_kw_arg(fn)
+        self._has_named_kw_args = has_named_kw_args(fn)
+        self._named_kw_args = get_named_kw_args(fn)
+        self._required_kw_args = get_required_kw_args(fn)
 
     async def __call__(self, request):
-        kw = #TODO
-        r = await self.func(**kw)
-        return r
+        kw = None
+        if self._has_var_kw_arg or self._has_named_kw_args of self._required_kw_args:
+            if request.method == 'POST':
+                if not request.content_type:
+                    return web.HTTPBadRequest("Missing Content-Type")
+                ct = request.content_type.lower()
+                if ct.startswith('application/json'):
+                    params = await request.json()
+                    if not isinstance(params, dict):
+                        return web.HTTPBadRequest('JSON body must be object.')
+                    kw = params
+                elif ct.startswith('appliction/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+                    params = await request.post()
+                    kw = dict(**params)
+                else:
+                    return webHTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
+
+            if request.method == 'GET':
+                qs = request.query_string
+                if qs:
+                    kw = dict()
+                    for k,v in parse.parse_qs(qs, True).items():
+                        kw[k] = v[0]
+        if kw is None:
+            kw = idct(**request.match_info)
+        else:
+            if not self._has_var_kw_arg and self._named_kw_args:
+                #remove all unamed kw:
+                copy = dict()
+                for name in self._named_kw_args:
+                    if name in kw:
+                        copy[name] = kw[name]
+                kw = copy
+            # check named arg:
+            for k,v in request.match_info.items():
+                if k in kw:
+                    logging.warning('Duplicate arg name in named ar and kw args:%s' % k)
+                kw[k] = v
+        if self._has_request_aarg:
+            kw['request'] = request
+        #ehcek require kw:
+        if self._required_kw_args:
+            for name in self._required_kw_args:
+                if not name in kw:
+                    return web.HTTPBadRequest('missing argument: %s' % name)
+        logging.info('call with args: %s' % str(kw))
+        try:
+            r = await self._func(**kw)
+            return r
+        expect APIError as e:
+            return dict(error=e.error, data=e.data, message=e.message)
+
 def add_route(app, fn):
     method = getattr(fn, '__method__', None)
     path = getattr(fn, '__route__', None)
